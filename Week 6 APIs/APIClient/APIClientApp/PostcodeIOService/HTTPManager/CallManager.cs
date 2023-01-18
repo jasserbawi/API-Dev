4bd9491efa39062393d65314a05b24bc499ff3a6
Jasser12544 <jasser.bawi19@gmail.com>
2023-01-18 15:22:32 +0000

    Commit

--- Week 6 APIs/APIClient/APIClientApp/PostcodeIOService/HTTPManager/CallManager.cs
+++ Week 6 APIs/APIClient/APIClientApp/PostcodeIOService/HTTPManager/CallManager.cs
@@ -2,4 +2,30 @@
 
 public class CallManager
 {
+    //Handles communication with the API
+    private readonly RestClient _client;
+    public RestResponse RestResponse { get; set; }
+
+    public CallManager()
+    {
+        _client = new RestClient(AppConfigReader.BaseUrl);
+    }
+
+    /// <summary>
+    /// defines and makes an API request, stores the response and returns the response content.
+    /// </summary>
+    /// <param name="postcode"></param>
+    public async Task<string> MakePostcodeRequestAsync(string postcode)
+    {
+        //Building the request
+        var request = new RestRequest(); //Makes the HTTP Request
+
+        request.Method = Method.Get;
+        request.AddHeader("Content-Type", "application/json");
+        request.Timeout = -1;
+
+        request.Resource = $"postcodes/{postcode}";
+
+        Response = await Client.ExecuteAsync(request);
+    }
 }
--- Week 6 APIs/APIClient/APIClientApp/PostcodeIOService/HTTPManager/SinglePostcodeService.cs
+++ Week 6 APIs/APIClient/APIClientApp/PostcodeIOService/HTTPManager/SinglePostcodeService.cs
@@ -1,6 +1,4 @@
-﻿using Newtonsoft.Json;
-
-namespace APIClientApp.PostcodeService;
+﻿namespace APIClientApp.PostcodeService;
 
 public class SinglePostcodeService
 {
