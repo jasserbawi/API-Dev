db33abe956da6c6036ac08279ea91f8dc0698f80
Jasser12544 <jasser.bawi19@gmail.com>
2023-01-23 11:29:04 +0000

    First commit

---
+++ Week 6 APIs/CustomerApp_MoqStarter/NorthwindTests/CustomerManagerShould.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Moq;
+using NorthwindBusiness;
+using NorthwindData;
+using NorthwindData.Services;
+using System.Data;
+
+namespace NorthwindTests
+{
+    public class CustomerManagerShould
+    {
+        private CustomerManager _sut;
+
+    }
+}
+
